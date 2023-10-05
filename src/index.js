beb9651664a564037c7614a6513e970c020a063a
Sveta Podsoblyaeva <sa.podsoblyaeva@yandex.ru>
2023-10-05 14:03:08 +0300

    feat: side menu и js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './scss/index.scss'
+
+import { SideMenu } from './components/SideMenu.js'
+
+import {
+  burgerButton,
+  sideMenu,
+  closeButton,
+  navLinks,
+} from './components/utils/constants.js'
+
+const sideMenuExample = new SideMenu(sideMenu)
+
+burgerButton.addEventListener('click', () => {
+  sideMenuExample.openSideMenu()
+})
+
+closeButton.addEventListener('click', () => {
+  sideMenuExample.closeSideMenu()
+})
+
+sideMenuExample.setEventListeners()
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', () => {
+    sideMenuExample.closeSideMenu()
+  })
+})
